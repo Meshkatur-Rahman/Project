43598790ee2a4dd18ebd73d2d88b832fc8871c26
Alamin <zkalamin22@gmial.com>
2019-07-17 22:16:52 +0600

    Project to test Second Step

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("In my Project");
+        System.out.println("In my Project Second Step");
     }
 }
